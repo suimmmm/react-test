24e9eb02ed09f29e342c0ae84a506d89404c10f3
suim <spdlqjtn159@gmail.com>
2022-04-23 21:09:35 +0900

    react 테스트 시작

---
+++ test/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // 메인페이지에 짜는 html
+
+    <div className="App">
+      <div className='black-nav'>
+        <div>개발 Blog</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
